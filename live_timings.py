8fe007ae3c6bf94055ad31d5d4c216c634e1ac81
mcgahern <61046390+mcgahern@users.noreply.github.com>
2022-01-12 19:52:19 +0000

    Delete live_timings.py

--- live_timings.py
+++
@@ -1,8 +0,0 @@
-import fastf1
-from fastf1.livetiming.data import LiveTimingData
-
-
-
-livedata = LiveTimingData('saved_data.txt')
-session = fastf1.get_session(2021, 'testing', 1)
-session.load_laps(with_telemetry=True, livedata=livedata)
